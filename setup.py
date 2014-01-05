0f728f91d59cc22885e872da72021cebad1cf670
Christoph Devenoges <c@devenoges.com>
2014-01-06 00:12:56 +1300

    formating

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 setup(name='orderbook',
-            version='0.1',
-            py_modules=['orderbook'],
-            )
+      version='0.1',
+      py_modules=['orderbook'],
+     )
